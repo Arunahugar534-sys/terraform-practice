1bb2b0cf55b5e6fd87325c54cb3767987e826e9c
Arunahugar534-sys <arunahugar48@gmail.com>
2026-03-14 00:09:36 +0530

    Terraform practice

--- dynamodb_sl/main.tf
+++ dynamodb_sl/main.tf
@@ -1,4 +1,12 @@
 resource "aws_s3_bucket" "name" {
-    bucket = "terraform-s3-bucket-practice-demo1"
+    bucket = "terraform-s3-bucket-practice-demo2"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.name.id
+
+    versioning_configuration {
+       status = "Enabled"
+    }
 }
 
